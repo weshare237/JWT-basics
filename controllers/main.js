32676c26766b8e5b8932a6c735f50bdcadda256e
weshare237 <fopaduclair2000@gmail.com>
2022-07-08 07:33:24 +0100

    Finshed

--- controllers/main.js
+++ controllers/main.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken')
-const CustomAPIError = require('../errors/custom-error')
+const { BadRequest } = require('../errors')
+const { StatusCodes } = require('http-status-codes')
 
 const login = async (req, res) => {
   const { username, password } = req.body
 
   if (!username || !password) {
-    throw new CustomAPIError('Please provide email and password', 400)
+    throw new BadRequest('Please provide email and password')
   }
 
   const id = new Date().getDate()
@@ -14,12 +15,12 @@ const login = async (req, res) => {
     expiresIn: '30d',
   })
 
-  res.status(200).json({ msg: 'user created', token })
+  res.status(StatusCodes.OK).json({ msg: 'user created', token })
 }
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100)
-  res.status(200).json({
+  res.status(StatusCodes.OK).json({
     msg: `Hello ${req.user.username}`,
     secret: `Here is your secret authorized data, your lucky number is ${luckyNumber}`,
   })
